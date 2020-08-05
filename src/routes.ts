d0866f288686767179f07e142bf1b64763d3c464
Danilo Barros <danilobjr@gmail.com>
2020-08-04 23:35:28 -0300

    feawt: post lesson

--- src/routes.ts
+++ src/routes.ts
@@ -1,14 +1,116 @@
 import express from 'express'
+import { db } from './database/connection'
 
 const routes = express.Router()
 
-routes.get('/', (_, response) => {
-    const fakeUsers = [
-        { id: 1, name: 'Danilo' },
-        { id: 2, name: 'Carminha' }
-    ]
+const convertTimeToMinutes = (time: string) => {
+  const [hours, minutes] = time
+    .split(':')
+    .map(Number)
 
-    return response.json(fakeUsers)
+  return hours * 60 + minutes
+}
+
+type ScheduleRequestBody = {
+  weekDay: number
+  from: string
+  to: string
+}
+
+type LessonPostRequestBody = {
+  name: string
+  avatar: string
+  whatsapp: string
+  bio: string
+  subject: string
+  cost: number
+  schedule: ScheduleRequestBody[]
+}
+
+const status = {
+  CREATED: 201,
+  SERVER_ERROR: 500
+}
+
+routes.post<any, any, LessonPostRequestBody>('/lessons', async (request, response) => {
+  const {
+    name,
+    avatar,
+    whatsapp,
+    bio,
+    subject,
+    cost,
+    schedule
+  } = request.body
+
+  const transaction = await db.transaction()
+
+  try {
+    type User = {
+      id: number
+      name: string
+      avatar: string
+      whatsapp: string
+      bio: string
+    }
+
+    type UserIds = User['id'][]
+
+    const usersTable = transaction<User>('users')
+
+    const [user_id] = await usersTable.insert<UserIds>({
+      name,
+      avatar,
+      whatsapp,
+      bio,
+    })
+
+    type Lesson = {
+      id: number
+      subject: string
+      cost: number
+      user_id: number
+    }
+
+    type LessonIds = Lesson['id'][]
+
+    const lessonsTable = transaction<Lesson>('lessons')
+
+    const [lesson_id] = await lessonsTable.insert<LessonIds>({
+      subject,
+      cost,
+      user_id
+    })
+
+    type LessonSchedule = {
+      week_day: number
+      from: number
+      to: number
+      lesson_id: number
+    }
+
+
+    const lessonSchedules = schedule
+      .map<LessonSchedule>(s => ({
+        week_day: s.weekDay,
+        from: convertTimeToMinutes(s.from),
+        to: convertTimeToMinutes(s.to),
+        lesson_id,
+      }))
+
+    const lessonScheduleTable = transaction<LessonSchedule>('lesson_schedule')
+    await lessonScheduleTable.insert(lessonSchedules)
+
+    await transaction.commit()
+
+    return response.status(status.CREATED).send()
+  } catch (error) {
+    await transaction.rollback()
+
+    return response.status(status.SERVER_ERROR).json({
+      error: 'Unexpected error on a database transaction'
+    })
+  }
 })
 
 export {
